0b784dd871a355e6e29b6cccd39dca35a61288a7
MarlinDayana <florezmarlin1996@gmail.com>
2021-11-14 20:37:23 -0500

    version1

---
+++ src/controllers/index.ts
@@ -0,0 +1,5 @@
+export * from './ping.controller';
+export * from './empresa-empleado.controller';
+export * from './empresa-proveedor.controller';
+export * from './empleado-empresa.controller';
+export * from './proveedor-empresa.controller';
---
+++ src/models/index.ts
@@ -0,0 +1,3 @@
+export * from './empleado.model';
+export * from './empresa.model';
+export * from './proveedor.model';
